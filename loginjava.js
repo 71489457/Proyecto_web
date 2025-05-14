9979be704be887699d705a8aeffeda821b018a77
Luis Esteban García Murga <70627297@CERTUS.EDU.PE>
2025-05-14 17:12:40 -0500

    Login y Register
    Arregle cambios y mejores practicas en mis archivos login y register tanto en HTML como en JavaScript.

--- loginjava.js
+++ loginjava.js
@@ -2,18 +2,10 @@ document.getElementById('login').addEventListener('submit',
     function(eventos) {
         eventos.preventDefault();
 
-        const usuarioIngresado = document.getElementById('Usuario').value; // Mejor nombre de variable
-        const contraseñaIngresada = document.getElementById('Contraseña').value; // Mejor nombre de variable
+        var usuarioLogin = document.getElementById('Usuario').value; // Mejor nombre de variable
+        var contraseñaLogin = document.getElementById('password_login').value; // Mejor nombre de variable
 
         //validacion de datos
-        if (usuarioIngresado && contraseñaIngresada) {
-            //guardado de datos
-            sessionStorage.setItem('Usuario', usuarioIngresado);
-            sessionStorage.setItem('Contraseña', contraseñaIngresada);
-
-            window.location.href = "Proyecto.html";
-        } else {
-            alert("completa los campos");
-        }
-    }
-);
+        if (usuarioLogin && contraseñaLogin) {
+           alert("no tienes una cuenta registrada");
+}});
